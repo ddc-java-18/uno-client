d427f888bb9c96e1db8b8f5b6c2b3972ebe7dc66
ivan-pd <ivanpinedadomingu@cnm.edu>
2024-07-29 19:49:34 -0600

    feat: Add UnoServiceProxy for handling http websevice requests.

---
+++ app/src/main/java/edu/cnm/deepdive/uno/hilt/UnoServiceModule.java
@@ -0,0 +1,68 @@
+package edu.cnm.deepdive.uno.hilt;
+
+import android.content.Context;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+import dagger.hilt.components.SingletonComponent;
+import edu.cnm.deepdive.uno.R;
+import edu.cnm.deepdive.uno.service.UnoServiceProxy;
+import javax.inject.Singleton;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+@InstallIn(SingletonComponent.class)
+public class UnoServiceModule {
+
+  /**
+   * Default initialization constructor for the {@code UnoServiceModule}
+   */
+  UnoServiceModule() {
+
+  }
+
+  /**
+   * Provides an UnoServiceProxy as a singleton using Hilt/Dagger dependency injection.
+   *
+   * <p>
+   *   This method initializes the app's GSON serializer, OkHttp web client, and provides them for
+   *   the creation of a Retrofit object.
+   * </p>
+   *
+   * @param context the app's ApplicationContext.
+   * @return UnoServiceProxy.
+   */
+  @Provides
+  @Singleton
+  public UnoServiceProxy provideProxy(@ApplicationContext Context context) {
+    Gson gson = new GsonBuilder()
+        .excludeFieldsWithoutExposeAnnotation()
+        .create();
+    String loggingLevelProperty = context.getString(R.string.logging_level).toUpperCase();
+    Level loggingLevel = Level.valueOf(loggingLevelProperty);
+    HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+    logging.setLevel(loggingLevel);
+
+    OkHttpClient client = new OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build();
+
+    Retrofit retrofit = new Retrofit.Builder()
+        .client(client)
+        .baseUrl(context.getString(R.string.base_url))
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .build();
+
+    return retrofit.create(UnoServiceProxy.class);
+  }
+
+}
---
+++ app/src/main/java/edu/cnm/deepdive/uno/service/UnoServiceProxy.java
@@ -0,0 +1,76 @@
+package edu.cnm.deepdive.uno.service;
+
+import edu.cnm.deepdive.uno.model.domain.Card;
+import edu.cnm.deepdive.uno.model.domain.Game;
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * The {@code UnoServiceProxy} interface defines the API endpoints for interacting with the UNO web
+ * game service.
+ */
+public interface UnoServiceProxy {
+
+  /**
+   * Creates a new game.
+   * <p>
+   * If a game exists on the web server matching the provided game parameters
+   * and is not yet at max capacity, the server returns the existing game and add the user as a player
+   * rather than creating a new game from scratch.
+   * </p>
+   *
+   * @param game the game object to be created.
+   * @param bearerToken the user's authorization token.
+   * @return a {@code Single<Game>} which emits the created Game.
+   */
+  @POST("games")
+  Single<Game> createGame(@Body Game game, @Header("Authorization") String bearerToken);
+
+  /**
+   * Starts an existing game
+   * @param gameKey the external key associated with the game to start.
+   * @param bearerToken the user's authorization token.
+   * @return a {@code Single<Game>} which emits the started Game.
+   */
+  @POST("games/{gameKey}/start")
+  Single<Game> startGame(@Path("gameKey") String gameKey,
+      @Header("Authorization") String bearerToken);
+
+  /**
+   * Submits a players move for validation to the UNO web service.
+   *
+   * @param gameKey the external key associated with the game for which the move is being submitted.
+   * @param bearerToken the user's authorization token.
+   * @param card the Card being submitted as a player's move
+   * @return a {@code Single<Game>} which emits the updated Game after the submitted move.
+   */
+  @POST("games/{gameKey}/moves")
+  Single<Game> submitMove(@Path("gameKey") String gameKey,
+      @Header("Authorization") String bearerToken,
+      @Body Card card);
+
+  /**
+   * Submit a player's request to draw a Card from the UNO discard pile.
+   *
+   * @param gameKey the external key associated with the game for which a card is being drawn for.
+   * @param bearerToken the user's authorization token.
+   * @return a {@code Single<Game>} which emits the updated Game after the card is drawn.
+   */
+  @POST("games/{gameKey}/draw")
+  Single<Game> drawCard(@Path("gameKey") String gameKey,
+      @Header("Authorization") String bearerToken);
+
+  /**
+   * Gets the Game from the UNO web service associated with the provided external gameKey.
+   *
+   * @param gameKey the external key associated with the game being retrieved.
+   * @return a {@code Single<Game>} which emits the Game associated with the external gameKey.
+   */
+  @GET("games/{gameKey}")
+  Single<Game> getGame(@Path("gameKey") String gameKey);
+
+}
